e981cc1d3613c22c2add4c10f1e437392d1fa2c7
Devanshi <devanshijoshi321@gmail.com>
2022-07-17 11:54:27 +0530

    Initial commit

--- E-StockMarket/src/app/app.component.ts
+++ E-StockMarket/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { AuthService } from './services/auth.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Flight_Booking';
+  title = 'E-StockMarket';
   constructor(private _auth: AuthService){}
   LoggedIn(input: boolean) {
     if (input) {
